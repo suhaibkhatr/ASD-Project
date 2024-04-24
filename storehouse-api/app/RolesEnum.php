b96104762e200ea9e4bce0ca2a11e2be28a86242
Suhaib Khater <s.khater@classera.com>
2024-04-24 13:47:02 -0500

    add role table and role to users table

---
+++ storehouse-api/app/RolesEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+enum RolesEnum: int
+{
+    case ADMIN = 1;
+    case SUPPLIER = 2;
+}
--- storehouse-api/database/seeders/DatabaseSeeder.php
+++ storehouse-api/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
+use App\RolesEnum;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(50)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            "name" => "Suhaib Khater",
+            "email" => "suhaib@test.com",
+            "role_id" => RolesEnum::ADMIN,
+        ]);
+
+        User::factory()->create([
+            "name" => "Test User",
+            "email" => "test@test.com",
+        ]);
+
+        DB::table("roles")->truncate();
+
+        Role::factory()->createMany([
+            ["title" => "admin"],
+            ["title" => "supplier"],
         ]);
     }
 }
