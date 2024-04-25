a308a7473f93c95e889bcfac0d7467fddec79125
Suhaib Khater <s.khater@classera.com>
2024-04-25 16:32:07 -0500

    add pivot table for users and storehouses

---
+++ storehouse-api/database/migrations/2024_04_25_212940_create_storehouse_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Storehouse;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("storehouse_users", function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, "user_id");
+            $table->foreignIdFor(Storehouse::class, "storehouse_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("storehouse_users");
+    }
+};
