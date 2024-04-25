af94299e717f5c62c35eb37c5dc5e5053b0a61b5
Suhaib Khater <s.khater@classera.com>
2024-04-25 16:20:08 -0500

    add store house types

--- storehouse-api/database/seeders/DatabaseSeeder.php
+++ storehouse-api/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Role;
+use App\Models\StorehouseType;
 use App\Models\User;
 use App\RolesEnum;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -37,6 +38,38 @@ class DatabaseSeeder extends Seeder
         Role::factory()->createMany([
             ["title" => "admin"],
             ["title" => "supplier"],
+        ]);
+
+        StorehouseType::factory()->createMany([
+            [
+                "title" => "Public Warehouse",
+                "description" => "A public warehouse is a storage facility that is owned and operated by a third-party logistics (3PL) company.
+                These warehouses offer storage space to businesses on a short-term or long-term basis.
+                Public warehouses are a good option for businesses that need flexible storage solutions
+                or that do not have the space to store their own inventory.",
+            ],
+            [
+                "title" => "Private Warehouse",
+                "description" => "A private warehouse is a storage facility that is owned and operated by a single company.
+                These warehouses are used to store the company's own inventory.
+                Private warehouses offer businesses more control over their inventory and can be customized to meet the specific needs of the business.",
+            ],
+            [
+                "title" => "Distribution Center",
+                "description" => "A distribution center is a warehouse that is used to store and distribute goods to customers.
+                Distribution centers are typically larger than public warehouses and have more sophisticated material handling systems.
+                They are often located near transportation hubs, such as airports or seaports.",
+            ],
+            [
+                "title" => "Cold Storage Warehouse",
+                "description" => "A cold storage warehouse is a warehouse that is designed to store perishable goods at a controlled temperature.
+                Cold storage warehouses are typically used to store food, beverages, and pharmaceuticals.",
+            ],
+            [
+                "title" => "Smart Warehouse",
+                "description" => "A smart warehouse is a warehouse that uses automation and technology to improve efficiency and productivity.
+                Smart warehouses often use robots to pick and pack orders, as well as sensors to track inventory levels.",
+            ],
         ]);
     }
 }
